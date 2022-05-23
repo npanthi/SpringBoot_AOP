6661d4af963114818d270672ff791ce26437ed24
Nisan Panthi <nisan.panthi@gm.com>
2022-05-23 13:57:02 -0500

    spring Employee service

---
+++ src/test/java/com/example/aopExample/AopExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aopExample;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
